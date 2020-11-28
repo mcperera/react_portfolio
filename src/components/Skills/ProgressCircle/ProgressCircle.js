69a43a4111f730186f29625a400697f042c38a27
Madushan Perera <madushanperera3817@gmail.com>
2020-11-28 12:54:23 +0530

    Skill percentage auto increment

--- src/components/Skills/ProgressCircle/ProgressCircle.js
+++ src/components/Skills/ProgressCircle/ProgressCircle.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./ProgressCircle.css";
 
 const ProgressCircle = ({ percentage, skillName }) => {
+  const [counter, setCounter] = useState(0);
+
+  function animateValue(start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+      //obj.innerHTML = Math.floor(progress * (end - start) + start);
+      setCounter(Math.floor(progress * (end - start) + start));
+      if (progress < 1) {
+        window.requestAnimationFrame(step);
+      }
+    };
+    window.requestAnimationFrame(step);
+  }
+
+  useEffect(() => animateValue(0, percentage, 1000), [percentage]);
+
   return (
     <>
       <div className='single-chart'>
@@ -21,7 +39,7 @@ const ProgressCircle = ({ percentage, skillName }) => {
           a 15.9155 15.9155 0 0 1 0 -31.831'
           />
           <text x='18' y='20.35' className='percentage'>
-            {percentage}%
+            {counter}%
           </text>
         </svg>
         <p>{skillName}</p>
