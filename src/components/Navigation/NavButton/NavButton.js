5873a3962c924d0f1310db813199e2951c4a1686
Madushan Perera <madushanperera3817@gmail.com>
2020-12-03 14:39:50 +0530

    Change Nav Button style

--- src/components/Navigation/NavButton/NavButton.js
+++ src/components/Navigation/NavButton/NavButton.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./NavButton.css";
 import logo from "../../../assets/logo/m512.png";
 
-const NavButton = ({ navClick, hideNavButton }) => {
+const NavButton = ({ handleClick, hideNavButton }) => {
+  const [showLogo, setShowLogo] = useState(true);
+
+  useEffect(() => {
+    let time = 5000;
+    if (showLogo) {
+      time = 5000;
+    } else {
+      time = 1000;
+    }
+    const changer = setInterval(() => {
+      setShowLogo((preState) => {
+        return !preState;
+      });
+    }, time);
+    return () => clearInterval(changer);
+  }, [showLogo]);
+
   return (
     <div
       className={hideNavButton ? `hide-nav-button` : `show-nav-button`}
-      onClick={navClick}
-    >
-      <img src={logo} alt='M-logo' className='logo' />
+      onClick={handleClick}>
+      {showLogo ? (
+        <img src={logo} alt="M-logo" className="logo" />
+      ) : (
+        <i className="fas fa-bars"></i>
+      )}
     </div>
   );
 };
