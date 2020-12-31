09a06b5440318813e9a39b8a800387a4d83f3474
Madushan Perera <madushanperera3817@gmail.com>
2020-12-31 23:00:12 +0530

    add new-year png

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-import christmast from "../../assets/merry-christmas.png";
+import newYear from "../../assets/new_year.png";
 
 import "./Work.css";
 
 const Work = () => (
   <div className="work-box">
     <h1 className="page-heading">Something Big is Coming..!</h1>
-    <img src={christmast} alt="chirstmas" />
+    <img src={newYear} alt="new-year" />
   </div>
 );
 
